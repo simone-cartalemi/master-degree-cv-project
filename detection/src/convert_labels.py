3756b88567f91abfb2f2872e41d8b6af213a22d0
simone-cartalemi <scsimocarta@gmail.com>
2024-10-01 21:48:19 +0200

    refactor: main for convert labels

--- detection/src/convert_labels.py
+++ detection/src/convert_labels.py
@@ -38,6 +38,9 @@ class MioDataset(Dataset):
         Prende il path di gt_train.csv in input e restituisce un dict del tipo {"img": [obj]}
         '''
 
+        if not labels_path.lower().endswith('.csv'):
+            raise "File label per dataset MIO-TCD non corretto"
+        
         labels_data = {}
         with open(labels_path, mode ='r') as f:
             csvFile = csv.reader(f)
@@ -92,6 +95,9 @@ class GramDataset(Dataset):
         Prende il path di una cartella che contiene file xml di annotazioni e restituisce un dict del tipo {"img": [obj]}
         '''
 
+        if not os.path.isdir(labels_path):
+            raise "Path label per dataset GRAM-RTM non corretto"
+        
         annotation_paths = sorted([f for f in os.listdir(labels_path) if f.lower().endswith('.xml')])
 
         labels_data = {}
@@ -100,63 +106,6 @@ class GramDataset(Dataset):
             labels_data[img_name] = self.read_xml(annotations)
         
         return labels_data
-        
-
-
-
-
-    def altra(self, labels_path):
-        for frame, label in tqdm(zip(frames, annotations)):
-            name = frame.replace(".jpg", ".txt")
-            objs = read_csv_data(ds_annotations + label)
-            output_objects = []
-            for obj in objs:
-                cls_index = list(vehicle_classes.values()).index(obj["class"])
-                cls = list(vehicle_classes.keys())[cls_index]
-                x_center = ((obj["bndbox"][0] + obj["bndbox"][2]) / 2) / frame_width
-                y_center = ((obj["bndbox"][1] + obj["bndbox"][3]) / 2) / frame_height
-                width = (obj["bndbox"][2] - obj["bndbox"][0]) / frame_width
-                height = (obj["bndbox"][3] - obj["bndbox"][1]) / frame_height
-                output_objects.append(f"{vehicle_classes[cls]} {x_center} {y_center} {width} {height}")
-            with open(input_folder + "labels/" + name, 'w') as file:
-                for item in output_objects:
-                    file.write(f"{item}\n")
-
-    def load_labels(self, labels_file):
-        tree = ET.parse(labels_file)
-        root = tree.getroot()
-        bbox = []
-        for child in root.iter('object'):
-            obj = {}
-            cls = child.find("class").text
-            if cls in self.VEHICLE_CLASSES:
-                obj["class"] = self.VEHICLE_CLASSES[cls]
-            elif cls == "motorbike":
-                obj["class"] = 1
-            elif cls == "big-truck":
-                obj["class"] = 2
-            else:
-                print("Classe non prevista:", cls)
-            obj["id"] = child.find("ID").text
-
-            bndbox = child.find('bndbox')
-
-            x1 = int(bndbox.find("xmin").text)
-            y1 = int(bndbox.find("ymin").text)
-            x2 = int(bndbox.find("xmax").text)
-            y2 = int(bndbox.find("ymax").text)
-            obj["bndbox"] = (x1, y1, x2, y2)
-            
-            bbox.append(obj)
-
-        return bbox
-
-
-
-def save_labels(output_file_path, annotations):
-    with open(output_file_path, 'w') as file:
-        for item in annotations:
-            file.write(f"{item}\n")
 
 
 
@@ -174,22 +123,32 @@ def yolo_format(image_path, objs):
         output_objects.append(f"{cls} {x_center} {y_center} {width} {height}")
     return output_objects
 
+def save_labels(output_file_path, labels):
+    with open(output_file_path, 'w') as file:
+        for item in labels:
+            file.write(f"{item}\n")
+
+
+
 
 if __name__ == "__main__":
-    full_data_path = int(sys.argv[1])
-    images_path = os.path.join(full_data_path, "/dataset/train/images/")
-    labels_path = os.path.join(full_data_path, "gt_train.csv")
-    labels_out_path = os.path.join(full_data_path, "/dataset/train/labels/")
+    dataset_name = int(sys.argv[1])
+    images_folder_path = int(sys.argv[2])
+    labels_path = int(sys.argv[3])
+    labels_out_path = os.path.join(images_folder_path, "../labels/")
     os.makedirs(labels_out_path, exist_ok=True)
 
-    labels_data = load_mio_labels(labels_path, VEHICLE_CLASSES_MIO)
-
-    # Ottieni la lista delle immagini
-    images = sorted([f for f in os.listdir(images_path) if f.lower().endswith('.jpg')])
-
-    for image in tqdm(images):
-        labels_file_name = image.replace('.jpg', '.txt')
-        
-        converted = yolo_format(images_path + image, labels_data[image.replace('.jpg', '')])
-
-        save_labels(labels_out_path + labels_file_name, converted)
+    Ds = None
+    if dataset_name == "mio-tcd":
+        Ds = MioDataset()
+    elif dataset_name == "gram":
+        Ds = GramDataset()
+    else:
+        raise "Nome dataset sconosciuto"
+    
+    labels = Ds.get_labels(labels_path)
+    for img_name, objs in tqdm(labels.items()):
+        img_path = os.path.join(images_folder_path, img_name + '.jpg')
+        new_labels_path = os.path.join(labels_out_path, img_name + '.txt')
+        new_labels = yolo_format(img_path, objs)
+        save_labels(new_labels_path, new_labels)
