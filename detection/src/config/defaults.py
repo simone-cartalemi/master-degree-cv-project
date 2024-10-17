2ba982a9954d5658685a2559616b289e5ad40d1c
simone-cartalemi <scsimocarta@gmail.com>
2024-10-17 18:53:58 +0200

    feat: update default variables

--- detection/src/config/defaults.py
+++ detection/src/config/defaults.py
@@ -1,19 +1,23 @@
 import numpy as np
 
 MODELS = ["v5m_gram", "v5m_mio", "v8s_mio"]
+V5_GRAM_WEIGHTS_PATH = "detection/weights/yolo v5/gram/best.pt"
+V5_MIO_WEIGHTS_PATH = "detection/weights/yolo v5/mio/best.pt"
+V8_MIO_WEIGHTS_PATH = "detection/weights/yolo v8/mio/best.pt"
 
 
 #BENCHMARK_PATH = "../datasets/thai/"
 VIDEO_RESOLUTION = (1920, 1080)
 VIDEO_FRAME_PER_SECOND = 120
 VIDEO_FORMAT = ".mov"
+MASK_PATH = "datasets/thai/masks/ROI_last.png"   # ROI_all.png
 
-HOMOGRAPHY_MATRIX = np.load("../homography/matrixes/thai_hom_matrix_last.npy")
+HOMOGRAPHY_MATRIX = np.load("homography/matrixes/thai_hom_matrix_last.npy")
 CM_PER_PIXEL_RATIO = 300 / 69
 VALIDATION_WINDOW = (800, 1070)
 SPEED_WINDOW = (200, 800)
 
-RESULTS_PATH = "../experiments/"
+RESULTS_PATH = "experiments/"
 
 EMISSION_COEFFICIENTS = {
     "diesel": (0.00580262, 0.00063457, 6.4831E-06, -1.446E-07, 2.3284E-09, -1.71E-11, 5.0932E-14),
