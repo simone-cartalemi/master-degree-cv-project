a80b9ca463ae1c3454c9306a33d0d849221b4f9d
simone-cartalemi <scsimocarta@gmail.com>
2024-10-20 13:25:21 +0200

    fix: update paths for training files

--- detection/launc_v5_commands.sh
+++ detection/launc_v5_commands.sh
@@ -1,10 +1,11 @@
+# Selezionare dataset di interesse
 dataset=mio|gram
 
 # Inizia il learning
-python detection/yolov5/train.py --img 640 --batch 64 --epochs 30 --freeze 10 --data "detection/src/dataset_$dataset.yaml" --weights "yolov5m.pt" --project "detection/weights/yolo v5/$dataset"
+python src/yolov5/train.py --img 640 --batch 64 --epochs 30 --freeze 10 --data "detection/dataset_$dataset.yaml" --weights "yolov5m.pt" --project "detection/weights/yolo v5/$dataset"
 
 # Continua il learning
-python detection/yolov5/train.py --resume --weights "detection/weights/yolo v5/$dataset/exp/weights/last.pt"
+python src/yolov5/train.py --resume --weights "detection/weights/yolo v5/$dataset/exp/weights/last.pt"
 
 # Valida i risultati
-python detection/yolov5/val.py --img 640 --batch-size 64 --weights "detection/weights/yolo v5/$dataset/exp/weights/best.pt" --data "detection/src/dataset_$dataset.yaml"
+python src/yolov5/val.py --img 640 --batch-size 64 --data "detection/dataset_$dataset.yaml" --weights "detection/weights/yolo v5/$dataset/exp/weights/best.pt"
--- detection/launch_v8_commands.sh
+++ detection/launch_v8_commands.sh
@@ -1,10 +1,9 @@
 
 # Inizia il learning
-yolo task=detect mode=train model=yolov8s.pt data=detection/src/dataset_mio.yaml batch=64 epochs=30 imgsz=640 plots=True project="detection/weights/yolo v8"
+yolo task=detect mode=train model=yolov8s.pt data=detection/dataset_mio.yaml batch=64 epochs=30 imgsz=640 plots=True project="detection/weights/yolo v8"
 
 # Continua il learning
 yolo train resume model="detection/weights/yolo v8/train/weights/last.pt"
 
-
 # Valida il modello
-yolo task=detect mode=val model="detection/weights/yolo v8/train/weights/best.pt" data=detection/src/dataset_mio.yaml
+yolo task=detect mode=val model="detection/weights/yolo v8/train/weights/best.pt" data=detection/dataset_mio.yaml
