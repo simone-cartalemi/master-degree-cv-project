d4ea4e9b7038e25c4fb51f334c6dfc2985490bcb
simone-cartalemi <scsimocarta@gmail.com>
2024-10-21 23:19:14 +0200

    feat: add exporting function

--- src/utils/fs.py
+++ src/utils/fs.py
@@ -1,5 +1,13 @@
 import json
+import csv
 
 def get_tracking(tracking_path: str) -> dict:
     with open(tracking_path) as f:
         return json.load(f)
+
+def export_speed_results(folder_path: str, name: str, header: list, data: list) -> None:
+    with open(folder_path + name + ".csv", mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(header)
+        for d in data:
+            writer.writerow(d)
