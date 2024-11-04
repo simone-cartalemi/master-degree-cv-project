98ebe4abe76f8bcc8a3d8ba418da4b2fe599dc26
simone-cartalemi <scsimocarta@gmail.com>
2024-11-04 20:54:51 +0100

    feat: add benchmark option to speeder

--- src/calculate_speed.py
+++ src/calculate_speed.py
@@ -8,7 +8,7 @@ from estimator.vehicles_manager import get_vehicles_dictionary
 from utils.fs import export_csv_lines, get_file_format_list, get_tracking
 
 
-def main(input_folder: str, output_folder: str):
+def main(input_folder: str, output_folder: str, benchmarking_labels: bool = False):
     results_list = get_file_format_list(input_folder, ".json")
     print(f"There are {len(results_list)} tracked videos")
 
@@ -31,7 +31,7 @@ def main(input_folder: str, output_folder: str):
             if speed_estimation is None:
                 continue
 
-            benchmark_speed = benchmark.find_vehicle_speed(video_name, vehicle_centers)
+            benchmark_speed = benchmark.find_vehicle_speed(video_name, vehicle_centers) if benchmarking_labels else ""
             min_f, max_f = benchmark.get_apparition_frames(vehicle_centers)
             exporting_data.append([video_file.rsplit('.', 1)[0], min_f, max_f, vehicle_class, speed_estimation, benchmark_speed])
 
@@ -42,7 +42,8 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("input_folder_path", type=str, help="Path of videos' track json file")
     parser.add_argument("output_folder_path", type=str, help="Path of output folder")
+    parser.add_argument("-b", "--benchmark", action="store_true", help="Add benchmarking to results")
 
     args = parser.parse_args()
 
-    main(args.input_folder_path, args.output_folder_path)
+    main(args.input_folder_path, args.output_folder_path, args.benchmark)
