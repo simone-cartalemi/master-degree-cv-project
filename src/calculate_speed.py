ac31a227f1deabe7fdad96a87dd13d757b1201ae
simone-cartalemi <scsimocarta@gmail.com>
2024-11-04 12:47:06 +0100

    refactor: renamed export csv function

--- src/calculate_speed.py
+++ src/calculate_speed.py
@@ -5,7 +5,7 @@ from validator.benchmark import Benchmark
 from config.defaults import BENCHMARK_LABELS_FILE, BENCHMARK_PATH
 from estimator.speed import linear_speed
 from estimator.vehicles_manager import get_vehicles_dictionary
-from utils.fs import export_speed_results, get_file_format_list, get_tracking
+from utils.fs import export_csv_lines, get_file_format_list, get_tracking
 
 
 def main(input_folder: str, output_folder: str):
@@ -35,7 +35,7 @@ def main(input_folder: str, output_folder: str):
             min_f, max_f = benchmark.get_apparition_frames(vehicle_centers)
             exporting_data.append([video_file.rsplit('.', 1)[0], min_f, max_f, vehicle_class, speed_estimation, benchmark_speed])
 
-        export_speed_results(output_folder, os.path.splitext(video_file)[0], ["File video", "min Frame", "max Frame", "vehicle class", "calculated speed", "ground thrut"], exporting_data)
+        export_csv_lines(output_folder, os.path.splitext(video_file)[0], ["File video", "min Frame", "max Frame", "vehicle class", "calculated speed", "ground thrut"], exporting_data)
 
 
 if __name__ == "__main__":
