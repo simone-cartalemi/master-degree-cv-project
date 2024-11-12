8c53269eb763564ef5fee46bcd8e6c0ceea76f59
simone-cartalemi <scsimocarta@gmail.com>
2024-11-12 19:56:06 +0100

    refactor: removed output path from arguments

--- src/calculate_speed.py
+++ src/calculate_speed.py
@@ -2,17 +2,18 @@ import os
 from argparse import ArgumentParser
 
 from validator.benchmark import Benchmark
-from config.defaults import BENCHMARK_LABELS_FILE, BENCHMARK_PATH
+from config.defaults import BENCHMARK_LABELS_FILE, BENCHMARK_PATH, RESULTS_PATH
 from estimator.speed import linear_speed
 from estimator.vehicles_manager import get_vehicles_dictionary
 from util.fs import export_csv_lines, get_file_format_list, get_tracking
 
 
-def main(input_folder: str, output_folder: str, benchmarking_labels: bool = False, verbose: bool = False):
+def main(input_folder: str, benchmarking_labels: bool = False, verbose: bool = False):
     results_list = get_file_format_list(input_folder, ".json")
     print(f"There are {len(results_list)} tracked videos")
 
     labels_path = os.path.join(BENCHMARK_PATH, BENCHMARK_LABELS_FILE)
+    output_folder = os.path.join(RESULTS_PATH, "speed", os.path.basename(os.path.dirname(input_folder)))
     benchmark = Benchmark(labels_path)
 
     for video_file in results_list:
@@ -42,10 +43,9 @@ def main(input_folder: str, output_folder: str, benchmarking_labels: bool = Fals
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("input_folder_path", type=str, help="Path of videos' track json file")
-    parser.add_argument("output_folder_path", type=str, help="Path of output folder")
     parser.add_argument("-b", "--benchmark", action="store_true", help="Add benchmarking to results")
     parser.add_argument("-v", "--verbose", action="store_true", help="Print output")
 
     args = parser.parse_args()
 
-    main(args.input_folder_path, args.output_folder_path, args.benchmark, args.verbose)
+    main(args.input_folder_path, args.benchmark, args.verbose)
