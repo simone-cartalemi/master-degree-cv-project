9a2c1beac09d0540b3443db383289f244849c9e0
simone-cartalemi <scsimocarta@gmail.com>
2024-11-12 21:01:13 +0100

    feat: calculate speed can elaborate file and folders

--- src/calculate_speed.py
+++ src/calculate_speed.py
@@ -8,19 +8,27 @@ from estimator.vehicles_manager import get_vehicles_dictionary
 from util.fs import export_csv_lines, get_file_format_list, get_tracking
 
 
-def main(input_folder: str, benchmarking_labels: bool = False, verbose: bool = False):
-    results_list = get_file_format_list(input_folder, ".json")
-    print(f"There are {len(results_list)} tracked videos")
+def main(resource_path: str, benchmarking_labels: bool = False, verbose: bool = False):
+    if os.path.isdir(resource_path):
+        folder_path = resource_path + "/"
+        traks_result_list = get_file_format_list(folder_path, ".json")
+    else:
+        folder_path = os.path.dirname(resource_path) + "/"
+        tracks_result_file_name = os.path.basename(resource_path)
+        traks_result_list = [tracks_result_file_name]
+
+    if verbose:
+        print(f"There are {len(traks_result_list)} tracked videos")
 
     labels_path = os.path.join(BENCHMARK_PATH, BENCHMARK_LABELS_FILE)
-    output_folder = os.path.join(RESULTS_PATH, "speed", os.path.basename(os.path.dirname(input_folder)))
+    output_folder = os.path.join(RESULTS_PATH, "speed", os.path.basename(os.path.dirname(folder_path)))
     benchmark = Benchmark(labels_path)
 
-    for video_file in results_list:
+    for video_file in traks_result_list:
         video_name = os.path.splitext(os.path.basename(video_file))[0]
         if verbose:
             print(f"Exporting speed estimation results in {video_name}")
-        video_tracks_path = os.path.join(input_folder, video_file)
+        video_tracks_path = os.path.join(folder_path, video_file)
 
         exporting_data = []
         history = get_tracking(video_tracks_path)
@@ -42,10 +50,10 @@ def main(input_folder: str, benchmarking_labels: bool = False, verbose: bool = F
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("input_folder_path", type=str, help="Path of videos' track json file")
+    parser.add_argument("resource_path", type=str, help="Path of videos' track json file or videos' track json folder")
     parser.add_argument("-b", "--benchmark", action="store_true", help="Add benchmarking to results")
     parser.add_argument("-v", "--verbose", action="store_true", help="Print output")
 
     args = parser.parse_args()
 
-    main(args.input_folder_path, args.benchmark, args.verbose)
+    main(args.resource_path, args.benchmark, args.verbose)
